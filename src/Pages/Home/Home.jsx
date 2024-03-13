e4c0c03b8695772e4043cf55c53a00e11796b0bd
rony <mehedihasanfeni558@gmail.com>
2024-03-13 23:00:39 +0600

    added navigation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Banner from "./Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-        
-            <h2>This is to home page</h2>
+           <Banner></Banner>
+            
         </div>
     );
 };
